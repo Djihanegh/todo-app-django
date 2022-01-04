3cf00d5bb0db1bdf1301bb14ce6f2193d4f1b526
Djihanegh <djihaneghilani@gmail.com>
2022-01-04 23:08:52 +0100

    created update task page

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from tasks.models import Task
+from .forms import *
 
 
 def index(request):
     tasks = Task.objects.all()
+    form = TaskForm()
 
-    context = {'tasks': tasks}
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'form': form}
 
     return render(
         request, 'tasks/list.html', context
     )
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+
+    return render(request, 'tasks/update_task.html')
